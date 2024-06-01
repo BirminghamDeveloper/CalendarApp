f057f6626766c8dc0d76579fb9e68470b8835c4b
Mustafa <smart1hashi@gmail.com>
2024-06-01 01:59:07 +0100

    Revert "+ second error commit"
    This reverts commit 8e6b1269bcf9ffa880f5036ee7f964d0729b3803.

--- app/src/main/java/com/hashinology/calendarapp/MainActivity.kt
+++ app/src/main/java/com/hashinology/calendarapp/MainActivity.kt
@@ -28,7 +28,6 @@ class MainActivity : AppCompatActivity() {
     private val appointments = mutableListOf<Appointment>()
     private var selectedDate = ""
 
-    2nd Error
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
